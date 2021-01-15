d60cbb646510a3625505eb967359d999b312c367
Erardo <erardoap@gmail.com>
2021-01-15 13:48:27 +0100

    AJAX practica

--- scripts/12-p1.js
+++ scripts/12-p1.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * Primero,
+ * Primero, declaramos nuestros enlaces de las APIS que usaremos
  */
 const BASE_URL = 'https://reqres.in/api/users/';
 const POSTMAN_URL = 'https://httpbin.org/post';
@@ -22,20 +22,28 @@ window.addEventListener('load', (ev) => {
   });
 });
 
+//método para limpiar los campos del user
 function clearFields() {
   document.querySelectorAll('span').forEach((element) => {
     element.innerHTML = '';
-    console.log(element);
+    //console.log(element);
   });
 }
 
-function procesarFetch(numsecs, user) {
-  let URL_DELAY = BASE_URL + user + '?delay=' + numsecs;
+/**
+ * Método async para procesar las llamadas fetch
+ * @param {*} numsecs numsecs establecido para el delay
+ * @param {*} user num user para la llamada
+ */
+async function procesarFetch(numsecs, user) {
+  //Los campos a rellenar
   let idUser = document.getElementById('id');
   let emailUser = document.getElementById('email');
   let firstName = document.getElementById('name');
   let status = document.getElementById('status');
 
+  //Hecho con promesas para entender mejor el procedimiento:
+  /*
   fetch(URL_DELAY, {
     method: 'GET',
     cache: 'no-cache',
@@ -48,13 +56,58 @@ function procesarFetch(numsecs, user) {
       }
     })
     .then((json) => {
-      console.log(json);
+      idUser.innerHTML = json.data.id;
+      emailUser.innerHTML = json.data.email;
     })
     .catch((error) => {
       console.error(error);
     });
-}
+    */
+  /***************/
+  //Aqui abrimos el try, antes de llamar o usar await
+  try {
+    //Añadiremos los datos introducidos en los campos a nuestra URL
+    let URL_DELAY = BASE_URL + user + '?delay=' + numsecs;
 
-/**
- * Segundo,
- */
+    //nos devolverá una respuesta, esperaremos con await y si algo va mal, saldrá el error en el catch
+    //PETICION GET
+    const response = await fetch(URL_DELAY, {
+      method: 'GET',
+      cache: 'no-cache',
+    });
+    //Si todo a ido bien, seguiremos obteniendo nuestro json, para después rellenar los campos ID y EMAIL del user con el JSON devuelto
+    if (response.ok) {
+      const json = await response.json();
+      idUser.innerHTML = json.data.id;
+      emailUser.innerHTML = json.data.email;
+
+      let datos = JSON.stringify(json.data);
+      //Aqui enviaremos los datos en formato JSON a nuestra URL,
+      // añadiremos la cabecera, enviamos un JSON
+      const responsePOST = await fetch(POSTMAN_URL, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: datos,
+        cache: 'no-cache',
+      });
+      //SI TODO VA BIEN, obtenemos los datos llegados desde el envio de datos,
+      // que es un JSON que procesamos y obtenemos el nombre del user, si algo va mal,
+      // lanzamos el estado de la peticion
+      if (responsePOST.ok) {
+        const jsonPOST = await responsePOST.json();
+        let datosPOST = JSON.parse(jsonPOST.data);
+        firstName.innerHTML = datosPOST.first_name;
+        status.innerHTML = responsePOST.status;
+      } else {
+        throw response.status;
+      }
+    } else {
+      throw response.status;
+    }
+    //En nuestro catch, capturamos el error lanzado anteriormente
+  } catch (error) {
+    status.innerHTML = error;
+  }
+}
